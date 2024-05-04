cdec2b7be0e627020a2bfd5b7b67d7740e5405d2
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-05-04 22:27:44 +0700

    menambahkan prosedur traverse untuk membaca isi node secara descending

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -121,6 +121,20 @@ bool listEmpty() {
     return (START == NULL);
 }
 
+void traverse() {
+    if (listEmpty()) {
+        cout << "\nList is empty." << endl;
+    }
+    else {
+        cout << "\nRecords in descending order or roll number are: " << endl;
+        Node* currentNode = START;   //step 1
+        while (currentNode != NULL) {  //step 2
+            cout << currentNode->noMhs << " " << currentNode->name << endl;   //step 3
+            currentNode = currentNode->next;   //step 4
+        }
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
