8a773daddbf2038d5df679f940bfd5cf4b00c775
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-05-04 22:41:39 +0700

    menambahkan prosedur untuk mencari data (searchData)

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -149,8 +149,25 @@ void revtraverse() {
             cout << currentNode->noMhs << " " << currentNode->name << endl;
             currentNode = currentNode->prev;
         }
+    }
+}
 
-
+void searchData() {
+    if (listEmpty() == true) {
+        cout << "\nList is empty." << endl;
+    }
+    Node* prev, * curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of student whose record you want to search: ";
+    int num;
+    cin >> num;
+    if (search(num, &prev, &curr) == false) {
+        cout << "\nRecord not found." << endl;
+    }
+    else {
+        cout << "\nRecord found." << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nName: " << curr->name << endl;
     }
 }
 
